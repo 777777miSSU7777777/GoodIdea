9011d2d94527eb25574c7a26425f48ba56401f4b
Oleg Fedorovich <6x7missu7x7@gmail.com>
2018-10-28 19:29:01 +0300

    Added some changes to static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,12 @@ class StaticPagesController < ApplicationController
     def index
         return 'index'
     end
+
+    def contacts
+        return 'contacts'
+    end
+
+    def about 
+        return 'about'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#index"
+  get '/contacts', to: 'static_pages#contacts'
+  get '/about', to: 'static_pages#about'
   
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
