fc3472bd5835bcd8bc5895962a59a28e6f6f037b
Oleg Fedorovich <6x7missu7x7@gmail.com>
2018-11-01 19:18:45 +0300

    Added delete action to comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,14 @@ class CommentsController < ApplicationController
         redirect_to project_path(@post)
     end
 
+    def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+
+        redirect_to project_path(@post)
+    end
+    
     private 
         def comment_params
             params.require(:comment).permit(:content)
