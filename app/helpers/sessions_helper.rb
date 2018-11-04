a3abaffb65403a9abcf02deb647f79f967c71845
Oleg Fedorovich <6x7missu7x7@gmail.com>
2018-11-05 02:43:44 +0300

    Added some security & no changes for new.html.erb

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,6 +20,11 @@ module SessionsHelper
           end
         end
       end
+    end
+
+    def current_user?(user)
+        user == current_user
+    end
     
 
     def logged_in?
@@ -37,4 +42,13 @@ module SessionsHelper
         session.delete(:user_id)
         @current_user = nil
     end
-end
+
+    def redirect_back_or(default)
+        redirect_to (session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
+    end
+
