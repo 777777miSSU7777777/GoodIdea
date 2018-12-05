052efe1df2fe1bad7a5743137481889a80e425b1
Oleg Fedorovich <6x7missu7x7@gmail.com>
2018-12-05 03:32:14 +0300

    Added validation for post & comment models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
     has_many :comments, dependent: :delete_all
+    validates :title, presence: true, length: { minimum: 5, maximum: 30}
+    validates :content, presence: true, length: { minimum: 500, maximum: 15000}
 end
