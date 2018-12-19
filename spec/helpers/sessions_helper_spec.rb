8bd327cffd9b89972d979a8eb1968d9cec06738b
Oleg Fedorovich <6x7missu7x7@gmail.com>
2018-12-19 07:02:54 +0300

    Fixed sessins helper's tests

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -25,25 +25,25 @@ RSpec.describe SessionsHelper do
         end
 
     describe "#log_in" do
-        it 'Log in user1 and compares id with user_id from session -> success' do
+        it 'Log in user1 and compares id with user_id from session -> equal' do
             log_in(@user1)
             expect(session[:user_id]).to eq @user1.id 
         end
 
-        it 'Log in user1 and compares user_id from session with user2 id -> fails' do
+        it 'Log in user1 and compares user_id from session with user2 id -> not equal' do
             log_in(@user1)
             expect(session[:user_id]).not_to eq @user2.id 
         end
     end
 
     describe "#remember" do
-        it 'Remembers user1 and checks user_id and remember_token from cookies -> success' do
+        it 'Remembers user1 and checks user_id and remember_token from cookies -> remembered' do
             remember(@user1)
             expect(cookies.permanent.signed[:user_id]).to eq @user1.id
             expect(cookies.permanent[:remember_token]).to eq @user1.remember_token
         end
 
-        it 'Remembers user1 and checks user_id and remember_token from user2 -> success' do
+        it 'Remembers user1 and checks user_id and remember_token from user2 -> differents remember tokens' do
             remember(@user1)
             expect(cookies.permanent.signed[:user_id]).not_to eq @user2.id
             expect(cookies.permanent[:remember_token]).not_to eq @user2.remember_token
@@ -52,24 +52,24 @@ RSpec.describe SessionsHelper do
 
 
     describe "#current_user" do
-        it 'Log in user1 and current_user returns same user -> success' do 
+        it 'Log in user1 and current_user returns same user -> logged correct user' do 
             log_in(@user1)
             expect(@user1).to eq current_user
         end
 
-        it 'Login and remember user1 and delete session to check cookie user search -> success' do
+        it 'Login and remember user1 and delete session to check cookie user search -> correct user deleted from session' do
             log_in(@user1)
             remember(@user1)
             session.delete(:user_id)
             expect(@user1).to eq current_user
         end
 
-        it 'Log in user1 and current_user compares with user2 -> fail' do 
+        it 'Log in user1 and current_user compares with user2 -> incorrect user' do 
             log_in(@user1)
             expect(@user2).not_to eq current_user
         end
 
-        it 'Remembers user1 but delete remember token from cookies to fail cookie user search -> fail' do
+        it 'Remembers user1 but delete remember token from cookies to fail cookie user search -> not in cookies not in session' do
             remember(@user1)
             cookies.delete(:remember_token)
             expect(nil).to eq current_user
@@ -77,30 +77,30 @@ RSpec.describe SessionsHelper do
     end
 
     describe "#current_user?" do
-        it 'Log in user1 and current_user? compares user1 & user1 -> success' do 
+        it 'Log in user1 and current_user? compares user1 & user1 -> correct user' do 
             log_in(@user1)
             expect(true).to eq current_user?(@user1)
         end      
 
-        it 'Log in user1 and current_user? compares user1 & user2 -> fail' do 
+        it 'Log in user1 and current_user? compares user1 & user2 ->  incorrect user' do 
             log_in(@user1)
             expect(false).to eql current_user?(@user2)
         end
     end
 
     describe "#logged_in?" do
-        it 'Log in and checks is user logged in? -> success' do 
+        it 'Log in and checks is user logged in? -> logged' do 
             log_in(@user1)
             expect(true).to eq logged_in?
         end
 
-        it 'Just tries to check user logged in? -> fail' do 
+        it 'Just tries to check user logged in? -> not logged' do 
             expect(false).to eq logged_in?
         end
     end
 
     describe "#forget" do
-        it 'Log in and forgets user1 -> success' do
+        it 'Log in and forgets user1 -> logged and forgot' do
             log_in(@user1)
             expect(true).to eq logged_in?
             forget(@user1)
@@ -108,13 +108,13 @@ RSpec.describe SessionsHelper do
             expect(nil).to eq cookies.permanent[:remember_token]
         end
 
-        it 'Just forgets current_user(w/o log in) -> fail' do
+        it 'Just forgets current_user(w/o log in) -> exception' do
             expect{ forget(current_user)}.to raise_exception(NoMethodError)
         end
     end
 
     describe "#log_out" do
-        it "Log in and log out -> success" do
+        it "Log in and log out -> correct" do
             log_in(@user1)
             expect(true).to eq logged_in?
             expect(@user1.id).to eq session[:user_id]
@@ -127,7 +127,7 @@ RSpec.describe SessionsHelper do
             expect(nil).to eq current_user
         end
 
-        it 'Just log out -> fail' do
+        it 'Just log out -> exception' do
             expect{ log_out }.to raise_exception(NoMethodError)
         end
     end
