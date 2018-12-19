5402567d561eed9e6cf8246380d71bc3b6ee9e15
Oleg Fedorovich <6x7missu7x7@gmail.com>
2018-12-19 05:45:48 +0300

    Refactoring & cleanup #2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Welcome to Good Idea!"
-      redirect_to user_path(@user)
+      redirect_to root_path
     else 
       render 'new'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,11 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   get '/profile/user/edit/:id', to: 'users#edit', as: "edit_profile"
   post '/profile/user/edit/:id', to: 'users#update'
-  get '/profile/users', to: 'users#index', as: "users"
-  get '/profile/user/:id', to: 'users#show', as: "user"
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  get '/projects/own', to: 'projects#own', as: "own_projects"
   get '/projects', to: 'projects#index', as: "projects"
   get '/projects/new', to: 'projects#new', as: "new_projects"
   post '/projects/new', to: 'projects#create'
@@ -31,5 +28,4 @@ Rails.application.routes.draw do
   resources :posts do 
     resources :donations
   end
-  
 end
