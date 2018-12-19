7698102babe6abf523b6dc9d7fd266fa6c0cd28f
Oleg Fedorovich <6x7missu7x7@gmail.com>
2018-12-19 03:55:17 +0300

    Changed validation in post mode

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
     belongs_to :user
     has_many :donations, dependent: :delete_all
-    validates :title, presence: true, length: { minimum: 5, maximum: 30}
-    validates :content, presence: true, length: { minimum: 500, maximum: 15000}
+    validates :title, presence: true, length: { maximum: 30}
+    validates :content, presence: true, length: { maximum: 15000}
     validates_numericality_of :goal, presence: true, greater_than_or_equal_to: 1
 
 end
